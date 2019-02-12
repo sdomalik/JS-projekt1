b57bbb65594a9277ba59aae205f785137cd55e3d
sdomalik <szymon.domalik@gmail.com>
2019-02-12 16:17:31 +0100

    added drawing with pen on canvas functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const input = document.querySelector('#fileInput')
+
 const brightness = document.querySelector("#brightness");
 const contrast = document.querySelector("#contrast");
 const saturation = document.querySelector("#saturation")
@@ -28,9 +29,9 @@ let satValue = 100;
 let conValue = 100;
 let brValue = 100;
 
-brightness.oninput = function(){
-    brValue = this.value
-    useFilters();
+brightness.oninput = function(){ //execute when input used
+    brValue = this.value // set brightness value
+    useFilters(); // execute function after change
 }
 contrast.oninput = function(){
     conValue = this.value
@@ -41,8 +42,72 @@ saturation.oninput = function(){
     useFilters();
 }
 
-function useFilters(){
+function useFilters(){ //seting filters on ctx whith values from inputs
     ctx.filter = `brightness(${brValue}%) contrast(${conValue}%) saturate(${satValue}%)`
     ctx.drawImage(img,0,0,1000,600)
 }
+
+//variables neccessary to draw on image
+let paint;
+let clickX = new Array();
+let clickY = new Array();
+let clickDrag = new Array();
+
+//function used for adding coordinates to array
+function addClick(x , y, dragging)
+{
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+}
+
+//execute when mouse button is pressed 
+canvas.addEventListener('mousedown', function(e){
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    draw();
+})
+//execute when cursor is moving on canvas
+canvas.addEventListener('mousemove', function(e){
+    if(paint){
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        draw();
+      }
+})
+//setting paint to false when mousse button is not pressed
+canvas.addEventListener('mouseup', function(e){
+    paint = false;
+})
+//setting paint to false when cursor is off the canvas
+canvas.addEventListener('mouseleave', function(e){
+    paint = false;
+})
+
+//drawing on canvas
+function draw(){
+    ctx.strokeStyle = "#df4b26";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 5;
+
+    for(let i=0; i < clickX.length; i++) {		
+        ctx.beginPath();
+        if(clickDrag[i] && i){
+          ctx.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           ctx.moveTo(clickX[i]-1, clickY[i]);
+         }
+         ctx.lineTo(clickX[i], clickY[i]);
+         ctx.closePath();
+         ctx.stroke();
+      }
+}
+
+
+
+
+
+
 
